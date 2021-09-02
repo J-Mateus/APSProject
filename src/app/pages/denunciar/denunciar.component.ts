b9cf130e1f3801cd4323abb9f01f2b7d7f32e657
José Matioli <j-mateusalves@hotmail.com>
2021-09-01 21:26:19 -0300

    Denúncia submit

--- src/app/pages/denunciar/denunciar.component.ts
+++ src/app/pages/denunciar/denunciar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CepMaskPipe } from 'src/app/pipes/cep-mask.pipe';
 import { TelMaskPipe } from 'src/app/pipes/tel-mask.pipe';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-denunciar',
@@ -15,7 +16,9 @@ export class DenunciarComponent implements OnInit {
   constructor(
     private telPipe: TelMaskPipe,
     private fb: FormBuilder,
-    private cepPipe: CepMaskPipe
+    private cepPipe: CepMaskPipe,
+    public alertController: AlertController,
+    public toastController: ToastController
     ) { }
 
   ngOnInit() {
@@ -33,12 +36,47 @@ export class DenunciarComponent implements OnInit {
 
   }
 
-
   updateWithTelMask(event) {
     this.formDenuncia.controls.tel.setValue(this.telPipe.transform(event.currentTarget.value));
   }
 
   updateWithCepMask(event) {
     this.formDenuncia.controls.cep.setValue(this.cepPipe.transform(event.currentTarget.value));
+  }
+
+  formSubmit() {
+    if(this.formDenuncia.valid){
+
+      console.log(this.formDenuncia.value);
+
+      this.presentToast();
+
+      this.formDenuncia.reset();
+    }else {
+      this.presentAlert();
+    }
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      message: 'Por favor, preencha todos os campos corretamente.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Denúncia cadastrada.',
+      color: "success",
+      duration: 3000,
+      position: 'top',
+      animated: true
+    });
+    toast.present();
   }
 }
